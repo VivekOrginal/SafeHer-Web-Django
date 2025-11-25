e9706f8656cf7840fd04e1c179616e32b553bdda
VivekOrginal <viveksubhash005@gmail.com>
2025-11-26 01:03:51 +0530

    Add Vercel deployment configuration

--- women_safety_platform/settings.py
+++ women_safety_platform/settings.py
@@ -122,11 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 AUTH_USER_MODEL = 'users.User'
 

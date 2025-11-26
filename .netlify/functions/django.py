e48444a7fe42e1476ff75e12df89ed34ce9f9702
VivekOrginal <viveksubhash005@gmail.com>
2025-11-26 21:13:27 +0530

    Add Netlify deployment configuration

---
+++ .netlify/functions/django.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from django.core.wsgi import get_wsgi_application
+
+# Add project to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'women_safety_platform.settings')
+
+application = get_wsgi_application()
+
+def handler(event, context):
+    return application(event, context)
